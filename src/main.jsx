d683a7ef5f895c814d7595d8e51b19887839ce84
lightrav3n <135107438+lightrav3n@users.noreply.github.com>
2024-01-26 12:01:17 +0100

    master

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // 2. import BrowserRouter
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
